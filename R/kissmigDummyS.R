624b8afc3d606709f1021c03e9304195e05b40b7
Michael P. Nobis <michael.nobis@wsl.ch>
2024-07-26 09:20:06 +0000

    version 1.0-5

--- R/kissmigDummyS.R
+++ R/kissmigDummyS.R
@@ -1,30 +1,29 @@
-#' Get suitability maps of Europe
+#' Get a simple suitability map
 #'
-#' \command{kissmigDummyS} is a support function to generate suitability maps of Europe for example code.
+#' \command{kissmigDummyS} is a support function to generate simple suitability maps based on mean annual air temperature for example code.
 #' @usage
-#' kissmigDummyS(mean, sd, download=FALSE)
+#' kissmigDummyS(mean, sd)
 #' @param mean temperature mean (degree celsius) of the suitability distribution
 #' @param sd temperature standard deviation (degree celsius) of the  suitability distribution
-#' @param download if TRUE, required climate data are downloaded from www.worldclim.org
 #'
 #' @details
-#' \command{kissmigDummyS} returns a suitability map of Europe based on mean annual temperature. It uses
-#' data of worldclim and calculates suitability as a normal distribution defined by \command{mean} and \command{sd}
-#' of mean annual temperature. The density function is linarely rescaled to a maximum of 1, the maximum suitability
-#' used in \command{kissmig}. Set \command{download=TRUE} to download the required climate data when running the
-#' function the first time.
+#' \command{kissmigDummyS} returns a suitability map for a given extent based on mean annual temperature. It uses
+#' data of WorldClim and calculates suitability as a normal distribution defined by \command{mean} and \command{sd}
+#' of mean annual temperature. The density function is linearly rescaled to a maximum of 1, the maximum suitability
+#' used in \command{kissmig}.
 #' @seealso \code{\link{kissmig}}
 #' @importFrom stats dnorm
+#' @import raster
 #' @export kissmigDummyS
 #' @references \url{https://www.worldclim.org/}
 
-kissmigDummyS <- function(mean, sd, download=FALSE) {
-  wcl <- getData('worldclim', var='bio', res=5, download=download) # download worldclim data
-  ans <- crop(raster(wcl, layer=1), extent(-12,38,28,72))          # mean annual temperature for Europe
-  if (((mean*10)<minValue(ans)) | ((mean*10)>maxValue(ans))) {
+kissmigDummyS <- function(mean, sd) {
+  ans <- wcl_bio1_europe
+  if (((mean)<minValue(ans)) | ((mean)>maxValue(ans))) {
     warning("mean outside temperature range")
   }
-  mean <- mean*10; sd <- sd*10
   values(ans) <- dnorm(values(ans), mean, sd)/dnorm(mean, mean, sd)
   return(ans)
 }
+
+utils::globalVariables("wcl_bio1_europe")
--- src/kissmig.c
+++ src/kissmig.c
@@ -18,7 +18,7 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 	SEXP arrN3;
 
 	int i, j, t, l, p, ps, p1, p2, nrow, ncol, nlay, n, b;
-	int *xarrT0, *xarrT1, *xarrN3; 
+	int *xarrT0, *xarrT1, *xarrN3;
 	double *xans, *xd, *xs;
 
 	int xit = INTEGER(it)[0];
@@ -31,14 +31,14 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 	ncol = INTEGER(dim)[1];
 	nlay = INTEGER(dim)[2];
 	n = nrow * ncol;
-	
+
 	PROTECT( d     = coerceVector(d, REALSXP));
 	PROTECT( s     = coerceVector(s, REALSXP));
 	PROTECT( ans   = allocVector(REALSXP, n) );
 	PROTECT( arrT0 = allocVector(LGLSXP , n) );
 	PROTECT( arrT1 = allocVector(LGLSXP , n) );
 	PROTECT( arrN3 = allocVector(LGLSXP , n) );
-	
+
 	xd     = REAL(d);
 	xs     = REAL(s);
 	xans   = REAL(ans);
@@ -47,43 +47,43 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 	xarrN3 = INTEGER(arrN3);
 
   	// R RNG
- 	GetRNGstate();	
+ 	GetRNGstate();
 
 	// read origin d into xarrlT1 and initialize xarrT0, xarrN3x3
-    for (i = 0; i < n; i++) { 
-		xarrT1[i] = (xd[i] == 1); 
+    for (i = 0; i < n; i++) {
+		xarrT1[i] = (xd[i] == 1);
 		xarrT0[i] = 0;
 		xarrN3[i] = 0;
 		xans[i]   = 0;
 	}
-	
+
 	// S layer loop (time periods)
 	for (t = 0; t < nlay; t++) {
-		
+
 		// iteration loop
 		for (l = 0; l < xit; l++) {
 
 			// t0 <- t1
 			for (i = 0; i < n; i++) { xarrT0[i] = xarrT1[i]; }
-			
+
 			// occurrences within 3x3 neighborhood (opt?)
-			p = ncol-1; 
+			p = ncol-1;
 			for (i = 1; i < nrow-1; i++) {
 				p++;
 				for (j = 1; j < ncol-1; j++) {
 					p++;
 					p1 = p+ncol;
 					p2 = p-ncol;
-					xarrN3[p] = 
+					xarrN3[p] =
 						(xarrT0[p ]) || (xarrT0[p -1]) || (xarrT0[p +1]) ||
-						(xarrT0[p1]) || (xarrT0[p1-1]) || (xarrT0[p1+1]) || 
+						(xarrT0[p1]) || (xarrT0[p1-1]) || (xarrT0[p1+1]) ||
 						(xarrT0[p2]) || (xarrT0[p2-1]) || (xarrT0[p2+1]);
 				}
 				p++;
 			}
 
 			// new distribution at T1
-			p  = ncol-1; 
+			p  = ncol-1;
 			ps = (t*n)+p;
 			for (i = 1; i < nrow-1; i++) {
 				p++; ps++;
@@ -91,25 +91,26 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 					p++; ps++;
 					p1 = p+ncol;
 					p2 = p-ncol;
-					
+
 					// potential colonization only if occurrences within 3x3 neighborhood
 					if (xarrN3[p]) {
-					
+
 						// extinction?
 						if (xarrT1[p]) { if (unif_rand()<=xpe) {xarrT1[p] = 0; } }
-					
+
 						// potential colonization only if focal cell uncolonized
+						b = 0;
 						if (!xarrT1[p]) {
-							b = 0;    if (xarrT0[p  ]) { b = (unif_rand() <= xs[ps]); }
+							          if (xarrT0[p  ]) { b = (unif_rand() <= xs[ps]); }
 							if (!b) { if (xarrT0[p-1]) { b = (unif_rand() <= xs[ps]); } }
 							if (!b) { if (xarrT0[p+1]) { b = (unif_rand() <= xs[ps]); } }
 							if (!b) { if (xarrT0[p1 ]) { b = (unif_rand() <= xs[ps]); } }
 							if (!b) { if (xarrT0[p2 ]) { b = (unif_rand() <= xs[ps]); } }
-						}	
+						}
 						// corner events
 						if (!b) {
 							if (unif_rand() <= xpc) {
-										  if (xarrT0[p1-1]) { b = (unif_rand() <= xs[ps]); } 
+										  if (xarrT0[p1-1]) { b = (unif_rand() <= xs[ps]); }
 								if (!b) { if (xarrT0[p1+1]) { b = (unif_rand() <= xs[ps]); } }
 								if (!b) { if (xarrT0[p2-1]) { b = (unif_rand() <= xs[ps]); } }
 								if (!b) { if (xarrT0[p2+1]) { b = (unif_rand() <= xs[ps]); } }
@@ -117,11 +118,11 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 						}
 						//
 						if ( b) { xarrT1[p] = 1; }
-					
+
 					}
 				}
 				p++; ps++;
-				
+
 			}
 
 			// calc (FCO=2; first colonization)
@@ -135,7 +136,7 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 
 		}
 
-	}	
+	}
 
 	// calc (DIS=1; final distribution)
 	if (xty == 1) { for (i = 0; i < n; i++) { xans[i] = xarrT1[i]; } }
@@ -146,7 +147,7 @@ SEXP kissmig_c(SEXP d, SEXP s, SEXP dim, SEXP it, SEXP pe, SEXP pc, SEXP ty, SEX
 	// finally
  	PutRNGstate();
 	UNPROTECT(6);
-	
+
 	return(ans);
 }
 
